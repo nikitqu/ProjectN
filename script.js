ca9594996c6cd285447c18bb54a43453711d925e
Nikita <example@gmail.com>
2022-02-16 17:26:41 +0300

    My first programm

---
+++ script.js
@@ -0,0 +1,22 @@
+const numberOfFilms = +prompt("сколько фильмов вы уже посмотрели?", '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies : {},
+    actor: {},
+    genres: [],
+    privat : false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ''),
+      b=  prompt("На сколько оцените его?", ''),
+      c= prompt("Один из последних просмотренных фильмов?", ''),
+      d = prompt("На сколько оцените его?", '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
+
+
